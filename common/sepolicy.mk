e5205f968063f212044241a882982054cf505338
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:35:47 +0200

    sepolicy: Initial repo structure

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Bliss
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/bliss/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Bliss
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/bliss/sepolicy/qcom
