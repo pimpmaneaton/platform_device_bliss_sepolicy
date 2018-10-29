a7d2a4dcdb678cccb643141f74be138e258d6a01
Jackeagle <jackeagle102@gmail.com>
2018-10-29 09:54:32 -0400

    sepolicy: gzosp --> bliss
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -4,10 +4,10 @@
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/gzosp/sepolicy/common/public
+    device/bliss/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/gzosp/sepolicy/common/private
+    device/bliss/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/gzosp/sepolicy/common/vendor
+    device/bliss/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -4,8 +4,8 @@
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/gzosp/sepolicy/qcom/private
+    device/bliss/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/gzosp/sepolicy/qcom/common \
-    device/gzosp/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/bliss/sepolicy/qcom/common \
+    device/bliss/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
