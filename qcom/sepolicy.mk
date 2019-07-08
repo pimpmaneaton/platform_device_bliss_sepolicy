8f3fcbbbee50b0aa07b9762bd188aa6240977c46
xlxfoxxlx <joshfox87@gmail.com>
2019-07-08 10:38:29 -0600

    sepolicy: update qcom/sepolicy.mk
    Change-Id: I2173dfa3b077b616d3be034fc2279aa5de72bd8b

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,8 +3,20 @@
 # that inherit from Lineage
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/bliss/sepolicy/qcom/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/bliss/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/bliss/sepolicy/qcom/dynamic \
     device/bliss/sepolicy/qcom/vendor
+endif
